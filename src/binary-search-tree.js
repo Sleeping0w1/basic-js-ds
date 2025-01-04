212ad409e63d015a93bab4c9877104e73e097496
Sleeping0w1 <1033351@gmail.com>
2025-01-04 23:49:24 +0300

    feat: solution for BinarySearchTree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,109 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
-
+  rootNode = null;
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this.rootNode === null) {
+      this.rootNode = new Node(data);
+    } else {
+      this.addNode(this.rootNode, new Node(data));
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addNode(rootNode, node) {
+    if (node.data < rootNode.data) {
+      if (rootNode.left === null) {
+        rootNode.left = node;
+      } else {
+        this.addNode(rootNode.left, node);
+      }
+    } else {
+      if (rootNode.right === null) {
+        rootNode.right = node;
+      } else {
+        this.addNode(rootNode.right, node);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.find(data) != null) return true;
+    else return false;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (data === this.rootNode.data) return this.rootNode;
+    else if (data < this.rootNode.data) return this.findNode(this.rootNode.left, data);
+    else return this.findNode(this.rootNode.right, data);
+  }
+  findNode(node, data) {
+    if (node.data === data) return node;
+    else if (data < node.data && node.left != null)
+      return this.findNode(node.left, data);
+    else if (data > node.data && node.right != null)
+      return this.findNode(node.right, data);
+    else return null;
+  }
+
+  remove(data) {
+    this.rootNode = this.removeNode(this.rootNode, data);
+  }
+  removeNode(node, data) {
+    if (node === null) return null;
+    else if (data < node.data) {
+      node.left = this.removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this.removeNode(node.right, data);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      } else if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      } else {
+        let newNode = this.find(this.minNode(node.right));
+        node.data = newNode.data;
+        node.right = this.removeNode(node.right, newNode.data)
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootNode.left === null) return this.rootNode.data;
+    else return this.minNode(this.rootNode.left);
+  }
+  minNode(node) {
+    if (node.left === null) return node.data;
+    else return this.minNode(node.left);
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootNode.right === null) return this.rootNode.data;
+    else return this.maxNode(this.rootNode.right);
+  }
+  maxNode(node) {
+    if (node.right === null) return node.data;
+    else return this.maxNode(node.right);
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
